340acbb66f564ec50523de7e650eadcce76d1f01
sagu29 <sagarnikhare1997@gmail.com>
2023-11-09 12:02:17 +0530

    updated the portfolio for further marks

--- README.md
+++
@@ -1,2 +0,0 @@
-# Welcome to my Portfolio 
-## Here is link : https://sagu29.github.io/
--- script.js
+++ script.js
@@ -1,73 +1,39 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector("#nav-menu");
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Developer", "Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+      
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+document.getElementById('resume-link-1').addEventListener("click", () => {
+  console.log("Script file loaded successfully");
+  window.open("https://drive.google.com/file/d/1hLDlhuaX22KHckAUQVnxO5DabjbjBz2x/view?usp=sharing", "_blank");
+});
+
+document.getElementById('resume-link-2').addEventListener("click", () => {
+  console.log("Script file loaded successfully");
+  window.open("https://drive.google.com/file/d/1hLDlhuaX22KHckAUQVnxO5DabjbjBz2x/view?usp=sharing", "_blank");
 });
