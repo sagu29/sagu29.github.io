865a3ae05945f30f62d2ae9f938b646e1b5c2f20
sagu29 <sagarnikhare1997@gmail.com>
2023-11-09 12:28:20 +0530

    added resume button in home section

--- script.js
+++ script.js
@@ -37,3 +37,8 @@ document.getElementById('resume-link-2').addEventListener("click", () => {
   console.log("Script file loaded successfully");
   window.open("https://drive.google.com/file/d/1hLDlhuaX22KHckAUQVnxO5DabjbjBz2x/view?usp=sharing", "_blank");
 });
+
+document.getElementById('resume-link-3').addEventListener("click", () => {
+  console.log("Script file loaded successfully");
+  window.open("https://drive.google.com/file/d/1hLDlhuaX22KHckAUQVnxO5DabjbjBz2x/view?usp=sharing", "_blank");
+});
